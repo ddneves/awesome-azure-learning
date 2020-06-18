177222836a8c9f079a74737de59ee6f7eb0aaf28
David das Neves <david_das_neves@gmx.de>
2020-06-18 14:28:16 +0200

    Updated output of QA.

--- DeadLinksAnalyzer.ps1
+++ DeadLinksAnalyzer.ps1
@@ -1,4 +1,4 @@
-﻿function Test-MarkdownLinks([String]$Path) {
+﻿function Test-MarkdownLinks([String]$Path, [Switch]$NoOutput) {
     $unreachable = @()    
 
     # Get markdown files recursively
@@ -6,18 +6,25 @@
 
     $files | ForEach-Object {
         $fileName = $_.Name
-        Write-Host "Analyzing '$fileName'" -ForegroundColor Yellow
+        if (-not $NoOutput) {
+            Write-Host "Analyzing '$fileName'" -ForegroundColor Yellow 
+        }
+
 
         $urls = Select-String -Path $_ -Pattern "\[.+\]\((http.*?)\)" | ForEach-Object { $_.matches.Groups[1] } | Select-Object
 
         $urls | ForEach-Object {
             $url = $_.Value
-            Write-Host "Requesting URL '$url'"
-
+            if (-not $NoOutput) {
+                Write-Host "Requesting URL '$url'"
+            }
+            
             try {
                 $request = Invoke-WebRequest -Uri $url -DisableKeepAlive -UseBasicParsing -Method head
             } catch {
-                Write-Warning -Message "Found dead URL '$url' in '$fileName'"
+                if (-not $NoOutput) {
+                    Write-Warning -Message "Found dead URL '$url' in '$fileName'"
+                }                
                 $linkHashtable = @{ 
                     Path = $fileName
                     BrokenURL = $url
@@ -36,12 +43,15 @@
 # Instantiating file
 $DeadLinks= @()
 
+# Output
+Write-Host "Action: Analyzing all files for broken links." -ForegroundColor Yellow 
+
 # Gathering dead links of all md files
-$DeadLinks = Test-MarkdownLinks -Path ".\"
+$DeadLinks = Test-MarkdownLinks -Path ".\" -NoOutput
 
 # Outputting dead links
 if ($DeadLinks) {
     Write-Host -Object '--- DEAD LINKS FOUND ---' -ForegroundColor Red
-    $DeadLinks | Format-Table
+    $DeadLinks | Format-Table -AutoSize | Out-String -Width 4096
     exit 1
 }
