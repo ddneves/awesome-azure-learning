e46504241ee217acafa64a172efcefc06336ff87
David das Neves <david_das_neves@gmx.de>
2020-07-24 10:30:17 +0200

    Suspended QA due to wrong scans from container

--- DeadLinksAnalyzer.ps1
+++ DeadLinksAnalyzer.ps1
@@ -1,28 +1,58 @@
 ﻿function Test-MarkdownLinks([String]$Path, [Switch]$NoOutput) {
+    # Adding necessary type
+    Add-Type -AssemblyName System.Net.Http
+
+    # Array to store all dead links
     $unreachable = @()    
 
     # Get markdown files recursively
     $files = Get-ChildItem -Path $Path -Recurse -Include "*.md"
 
+    # Running through all files in repository
     $files | ForEach-Object {
         $fileName = $_.Name
         if (-not $NoOutput) {
             Write-Host "Analyzing '$fileName'" -ForegroundColor Yellow 
         }
 
-
+        # Retrieving all URLs of page
         $urls = Select-String -Path $_ -Pattern "\[.+\]\((http.*?)\)" | ForEach-Object { $_.matches.Groups[1] } | Select-Object
 
+        # Running through all URLs to retrieve status code
         $urls | ForEach-Object {
             $url = $_.Value
             if (-not $NoOutput) {
                 Write-Host "Requesting URL '$url'"
             }
             
-            try {
-                $request = Invoke-WebRequest -Uri $url -TimeoutSec 12
-            } catch {
+            $winhttpclient = New-Object System.Net.Http.HttpClient
+            $winhttpclient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0"); 
+            Write-Host "Start Request"
+            $winhttptask = $winhttpclient.GetStringAsync($URL)
+            
+            Try 
+            {
+               Write-Host "Wait für completion"
+               $winhttptask.wait() # wait für end of http-tasks
+               Add-Member -InputObject $winhttptask -Name LastStatus -MemberType noteproperty -Value "OK" -Force
+            }
+            Catch 
+            {
+               Write-Host "Download ERROR $URL"
+               Write-Host ("Error:" + $winhttptask.Exception.InnerException.message)
+               Add-Member -InputObject $winhttptask -Name LastStatus -MemberType noteproperty -Value ($winhttptask.Exception.InnerException.message) -Force
+               $error.removerange(0,1) # remove last error
+            }
+            
+            if ($winhttptask.LastStatus -eq "OK") 
+            {
                 if (-not $NoOutput) {
+                    Write-Host "Resolved URL '$url' in '$fileName' successfully." -ForegroundColor Green
+                }
+            }
+            else {
+                if (-not $NoOutput) 
+                {
                     Write-Warning -Message "Found dead URL '$url' in '$fileName'"
                 }                
                 $linkHashtable = @{ 
@@ -47,7 +77,7 @@ $DeadLinks= @()
 Write-Host "Action: Analyzing all files for broken links." -ForegroundColor Yellow 
 
 # Gathering dead links of all md files
-$DeadLinks = Test-MarkdownLinks -Path ".\" -NoOutput
+$DeadLinks = Test-MarkdownLinks -Path ".\" #-NoOutput
 
 # Outputting dead links
 if ($DeadLinks) {
